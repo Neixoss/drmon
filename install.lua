e0cb02957a56aac21a658af0d13813c826d2c335
Erik Kõiv <koiverik@gmail.com>
2024-11-12 02:21:46 +0200

    Install link update

--- install.lua
+++ install.lua
@@ -2,8 +2,8 @@
 --
 --
 
-local libURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/drmon.lua"
+local libURL = "https://raw.githubusercontent.com/erkoiv/drmon/master/lib/f.lua"
+local startupURL = "https://raw.githubusercontent.com/erkoiv/drmon/master/drmon.lua"
 local lib, startup
 local libFile, startupFile
 
@@ -12,7 +12,7 @@ fs.makeDir("lib")
 lib = http.get(libURL)
 libFile = lib.readAll()
 
-local file1 = fs.open("lib/f", "w")
+local file1 = fs.open("lib/f.lua", "w")
 file1.write(libFile)
 file1.close()
 
@@ -20,7 +20,7 @@ startup = http.get(startupURL)
 startupFile = startup.readAll()
 
 
-local file2 = fs.open("startup", "w")
+local file2 = fs.open("startup.lua", "w")
 file2.write(startupFile)
 file2.close()
 
